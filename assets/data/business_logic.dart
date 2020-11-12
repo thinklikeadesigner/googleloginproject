cb2ee0d376e7b3e43fba4c449afcde0c04061632
rebecca burch <rebecca.burch.stu@gmail.com>
2020-11-12 08:46:34 -0600

    google sign in, log out business logic and pubspec files and assets

---
+++ assets/data/business_logic.dart
@@ -0,0 +1,33 @@
+// {
+//   "person": {
+//     "id": 2345234,
+//     "first name": "john"
+//     "last name": "doe"
+//     "email address": "john.doe@yahoo.com,"
+//     "accumulated points": 300
+//     "created at": "2014-01-01T23:28:56.782Z"
+//     "last notified at": "2014-01-01T23:28:56.782Z"
+//     "activities": {
+//       "run": {
+//         "points": 5
+//         "completed": true
+//       },
+//       "meditate": {
+//         "points": 5
+//         "completed": false
+//       },
+//       "knit": {
+//         "points": 5
+//         "completed": false
+//       },
+//       "coffee break": {
+//         "points": 5
+//         "completed": true
+//       },
+//       "call a friend": {
+//         "points": 5
+//         "completed": false
+//       },
+//     }
+//   }
+// }
